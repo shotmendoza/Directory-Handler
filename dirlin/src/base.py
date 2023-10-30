73eb6c942d3dfbba8c4f1dc7fccb354c6538ed2f
mendozasho <shomendoza@gmail.com>
2023-10-29 23:22:31 -0700

    Added functionality for document class

--- dirlin/src/base.py
+++ dirlin/src/base.py
@@ -6,6 +6,48 @@ from typing import Optional
 import pandas as pd
 
 
+class Document:
+    def __init__(self, df: pd.DataFrame, path: Path):
+        self.dataframe: pd.DataFrame = df
+        self.filepath: Path = path
+
+    def check_columns(self, headers: list[str], match_all: bool = True, raise_error: bool = True) -> bool:
+        """
+        Checks the dataframe to confirm that expected columns are in the data.
+
+        :param headers: the headers to check for
+        :param match_all: defaults to confirm every column is in data. When False, confirms if any columns is in data
+        :param raise_error: defaults to raising error for missing column. When False, only returns bool
+        :return: True if all columns are present. False if any or all columns are missing (param dependent)
+        """
+        if match_all is True:
+            if any([h in self.dataframe.columns for h in headers]) is False:
+                if raise_error:
+                    raise KeyError(f"Missing on or all expected columns {headers} in {self.filepath.stem} file.")
+                return False
+            return True
+
+        if any([h in self.dataframe.columns for h in headers]) is True:
+            return True
+        else:
+            if raise_error:
+                raise KeyError(f"Missing all expected columns {headers} in {self.filepath.stem} file.")
+            return False
+
+    def move_file(self, destination: Path):
+        """
+        Moves the current file you are working with to another folder or destination.
+
+        :param destination: the full path (including filename) of the Path object
+        :return: the new path (also changes state of object in self.filepath)
+        """
+        try:
+            self.filepath = self.filepath.rename(target=destination)
+        except Exception as e:
+            raise e
+        return self.filepath
+
+
 class Folder:
     def __init__(self, folder_path: Path | str):
         """Object used for processing files through local directories.
@@ -32,6 +74,48 @@ class Folder:
         if not self.path.is_dir():
             raise ValueError(f"Expected a path to a folder / directory. Got {self.path}.")
 
+    def _find_recent_files(
+            self,
+            filename_pattern: str,
+            days: int = 30,
+            with_asterisks: bool = True,
+            recurse: bool = False) -> Path:
+        """
+        Base function for finding the most recent file.
+        Used in open_recent() and open_recent_as_document()
+
+        :param filename_pattern:
+        :param days:
+        :param with_asterisks:
+        :param recurse:
+        :return:
+        """
+
+        asterisks_mapping = {
+            True: f"{filename_pattern}*",
+            False: filename_pattern
+        }
+
+        if recurse:
+            files = [
+                f for f in self.path.rglob(pattern=f"{asterisks_mapping[with_asterisks]}")
+                if date.fromtimestamp(f.stat().st_mtime) >= (date.today() - timedelta(days=days))
+                and not f.name.startswith("~")
+            ]
+        else:
+            files = [
+                f for f in self.path.glob(pattern=f"{asterisks_mapping[with_asterisks]}")
+                if date.fromtimestamp(f.stat().st_mtime) >= (date.today() - timedelta(days=days))
+                and not f.name.startswith("~")
+            ]
+
+        try:
+            most_recent_file = Path(sorted(files, key=os.path.getmtime, reverse=True)[0])
+            return most_recent_file
+        except IndexError:
+            raise IndexError(
+                f"No reports found in '{self.path.parent.name}/{self.path.name}' directory in the past {days} days.")
+
     @classmethod
     def open(cls, file_path: str | Path, *args, **kwargs) -> pd.DataFrame:
         """
@@ -55,6 +139,11 @@ class Folder:
         else:
             raise KeyError(f"File suffix {file_path.suffix} is an unsupported format.")
 
+    @classmethod
+    def open_as_document(cls, file_path: str | Path, *args, **kwargs) -> Document:
+        df = cls.open(file_path, *args, **kwargs)
+        return Document(df=df, path=file_path)
+
     def open_recent(
             self,
             filename_pattern: str,
@@ -73,30 +162,29 @@ class Folder:
         :param kwargs: named arguments for the DataFrame object
         :return: a DataFrame object
         """
-        asterisks_mapping = {
-            True: f"{filename_pattern}*",
-            False: filename_pattern
-        }
+        most_recent_file = self._find_recent_files(filename_pattern, days, with_asterisks, recurse)
+        return self.open(most_recent_file, *args, **kwargs)
 
-        if recurse:
-            files = [
-                f for f in self.path.rglob(pattern=f"{asterisks_mapping[with_asterisks]}")
-                if date.fromtimestamp(f.stat().st_mtime) >= (date.today() - timedelta(days=days))
-                and not f.name.startswith("~")
-            ]
-        else:
-            files = [
-                f for f in self.path.glob(pattern=f"{asterisks_mapping[with_asterisks]}")
-                if date.fromtimestamp(f.stat().st_mtime) >= (date.today() - timedelta(days=days))
-                and not f.name.startswith("~")
-            ]
+    def open_recent_as_document(
+            self,
+            filename_pattern: str,
+            days: int = 30,
+            with_asterisks: bool = True,
+            recurse: bool = False, *args, **kwargs) -> Document:
+        """
+        Please refer to documentation on open_recent().
+        The difference is that this function returns a Document object.
 
-        try:
-            most_recent_file = Path(sorted(files, key=os.path.getmtime, reverse=True)[0])
-            return self.open(most_recent_file, *args, **kwargs)
-        except IndexError:
-            raise IndexError(
-                f"No reports found in '{self.path.parent.name}/{self.path.name}' directory in the past {days} days.")
+        :param filename_pattern: the naming convention for the file you are searching
+        :param days: number of days back you would like to search
+        :param with_asterisks: whether to append an asterisks at the end of the filename_pattern
+        :param recurse: whether to recurse through sub-folders
+        :param args: arguments for dataframe function
+        :param kwargs: keyword arguments for dataframe function
+        :return:
+        """
+        most_recent_file = self._find_recent_files(filename_pattern, days, with_asterisks, recurse)
+        return self.open_as_document(most_recent_file, *args, **kwargs)
 
     def as_map(
             self,
