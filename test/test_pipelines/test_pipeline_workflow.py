22c51b27e8f9eac5601b9eba2c2a1a2e13772f25
mendozasho <shomendoza@gmail.com>
2025-01-01 00:49:50 -0800

    bug fixes on Validation and Check
    1. fixed bug where check functions without types were  missing from expected_args
    2. Added more checks

--- test/test_pipelines/test_pipeline_workflow.py
+++ test/test_pipelines/test_pipeline_workflow.py
@@ -1,23 +1,43 @@
 import pytest
 
 from dirlin.pipeline import Check, Validation
-from test.test_pipelines.test_pipeline_fixtures import TestSharedParamSingleField
+from test.test_pipelines.test_pipeline_fixtures import single_stock_df, std_check_function, two_stock_df
 
-_test_cases = [
-    TestSharedParamSingleField,
+_shared_param_with_single_field = single_stock_df, std_check_function
+"""testing class aims to test use cases where a test function has a shared parameter,
+but the field in the DataFrame only has one field that is under the shared parameter.
+
+For example, if a function has parameters(id, based_shared_field, static_field), then
+we would expect that a dataframe that only had one column under that shared parameter (because
+currently, the logic dictates that if no matching name is found, look at the base
+of the word `shared_field`) would be a static field.
+
+An example that got caught: `check_function_net_income(stock_gross_income, stock_expenses)`.
+This was passed to a DataFrame that only had the following fields: `tsm_gross_income`, `tsm_expenses`.
+Don't remember the error code but would want to catch these types of errors.
+"""
+
+
+_test_check_cases = [
+    _shared_param_with_single_field,
+    (two_stock_df, std_check_function),
 ]
 
 
-@pytest.mark.parametrize("validation_use_case", _test_cases)
-def test_pipeline_validation_workflow(validation_use_case):
-    # i) setting up the test
-    current_test_case = validation_use_case()
+@pytest.mark.parametrize("data, check", _test_check_cases)
+def test_pipeline_validation_workflow(data, check):
+    """parameterizing the actual check types with different types of checks
 
+    :param check: the different types of check functions we might see in the wild
+    :param data: just a standard DF we can use
+    :return:
+    """
     # ii) creating the pipeline
-    check = Check(current_test_case.check_function)
+    check = Check(std_check_function)
     validation = Validation(check)
-    df = current_test_case.create_example_df()
+    df = data()
 
     # iii) Currently running it through pipeline since Pipeline is a little messed up
     result = validation.run(df, infer_shared=True)
+    el = validation.generate_error_log()
     assert result is not None
