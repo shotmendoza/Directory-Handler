f96245a543aab8ea51dff7890c8e933e9f0b09c4
mendozasho <shomendoza@gmail.com>
2025-05-02 13:07:22 -0700

    bug fixes and new features
    1. Updated requirements.txt
    2. updated logic for mapping parameters to columns
    3. updated logic for get_all_functions_in_class
    4. alias_mapping properly pulls from all previous subclasses without overwriting

--- test/test_base.py
+++ test/test_base.py
@@ -1,4 +1,6 @@
-from dirlin import Directory
+import pandas as pd
+
+from dirlin import Directory, BaseValidation
 
 
 def test_init_on_mac():
@@ -8,4 +10,40 @@ def test_init_on_mac():
     d = Directory()
     print(d.folder)
 
-# why no tracking?
+
+def function_1(foo: int, bar: int, foobar: int) -> int:
+    return foo + bar + foobar
+
+
+def example_df() -> pd.DataFrame:
+    temp = {"bee": [1, 2, 3], "bar": [4, 5, 6], "foobar": [7, 8, 9]}
+    return pd.DataFrame.from_dict(temp)
+
+
+def example_two_df() -> pd.DataFrame:
+    temp = {"bee": [1, 2, 3], "brr": [4, 5, 6], "far": [7, 8, 9]}
+    return pd.DataFrame.from_dict(temp)
+
+
+class TestBaseValidation(BaseValidation):
+    first_function = function_1
+    alias_mapping = {"foo": ["bee"]}
+
+
+class TestSecondLayer(TestBaseValidation):
+    first_function = function_1
+    alias_mapping = {"bar": ["brr"], "foobar": ["far"]}
+
+
+def test_class_works_as_expected():
+    t_bv = TestBaseValidation()
+    t_df = example_df()
+    t_bv.run_validation(t_df)
+
+
+def test_second_layer_works():
+    t_bv = TestSecondLayer()
+    t_df = example_two_df()
+    t_bv.run_validation(t_df)
+
+    print(t_bv.alias_mapping)
