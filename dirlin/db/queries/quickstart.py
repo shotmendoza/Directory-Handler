9244f52b5f63b31adbd9a23a98bc23ba51330759
mendozasho <shomendoza@gmail.com>
2025-06-14 22:20:14 -0700

    Adding template for sqa db setup

--- dirlin/db/queries/quickstart.py
+++ dirlin/db/queries/quickstart.py
@@ -14,12 +14,17 @@ class CreateOrUpdateRecord(Query):
         """
         self.setup = setup
 
-    def execute(self, table: type[DeclarativeBase], model: BaseModel) -> None:
+    def execute(
+            self,
+            table: type[DeclarativeBase],
+            model: BaseModel,
+            table_id_field: str,
+            model_id_field: str,
+    ) -> None:
         with self.setup.session.begin() as sesh:
-            # (!) probably need to check whether the table has transaction_id
             query = sesh.execute(
                 select(table)
-                .where(table.iid == model.iid)  # type:ignore
+                .where(getattr(table, table_id_field) == getattr(model, model_id_field))  # type:ignore
             )
             if query.one_or_none() is None:
                 new_record = table(**model.model_dump(exclude_none=True))
@@ -27,7 +32,7 @@ class CreateOrUpdateRecord(Query):
             else:
                 sesh.execute(
                     update(table),
-                    model.model_dump(exclude={"transaction_id": True}, exclude_none=True)
+                    model.model_dump(exclude={model_id_field: True}, exclude_none=True)
                 )
 
 
@@ -42,11 +47,12 @@ class ReadRecordWithTransactionID(Query):
             self,
             table: DeclarativeBase,
             model: BaseModel,
-            transaction_id: str
+            id_lookup: str,
+            table_id_field: str,
     ) -> BaseModel:
         with self.setup.session.begin() as sesh:
             query = sesh.execute(
-                select(table).where(table.transaction_id == transaction_id)  # type:ignore
+                select(table).where(getattr(table, table_id_field) == id_lookup)  # type:ignore
             )
             return model.model_validate(query.scalar_one())
 
